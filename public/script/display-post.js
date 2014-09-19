5340ef73fb9b84810c86f1d23b4790bb942584b9
Spike Nox <spike01@gmail.com>
2014-09-19 12:15:51 +0100

    Almost at a live version

--- public/script/display-post.js
+++ public/script/display-post.js
@@ -1,6 +1,6 @@
 $(document).ready( function() {
   $('#test-post').on('submit', function(e) {
     e.preventDefault();
-    $('.message-container').prepend('<h1>'+$('#name').val()+'</h1><h2>'+$('#cohort').val()+'</h2><p>'+$('#message').val()+'</p>')
+    $('.message-container').prepend('<h1>'+$('#name').val()+'</h1><h2>'+$('#cohort').val()+'</h2><p>'+$('#message').val()+'</p><br>')
   }); 
 });
