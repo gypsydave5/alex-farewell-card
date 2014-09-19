a2c43e84ae84a977ea933337461998fb8f5aaee9
Ethel Ng <ethel.ng@gmail.com>
2014-09-19 15:48:28 +0100

    Modal for form input

--- script/interface.js
+++ script/interface.js
@@ -2,9 +2,11 @@ $(window).scroll(function() {
   if ($(this).scrollTop() > 1){  
     $('header').addClass("sticky");
     $('.message-container').addClass("sticky");
+    $('#about').addClass("sticky");
   }
   else{
     $('header').removeClass("sticky");
     $('.message-container').removeClass("sticky");
+    $('#about').removeClass("sticky");
   }
 });
