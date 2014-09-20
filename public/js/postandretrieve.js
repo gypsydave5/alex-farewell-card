1ad176716f414c5af3331f314a6f2e68fa49f7a5
Ethel Ng <ethel.ng@gmail.com>
2014-09-20 16:29:31 +0100

    Generating random colours for tiles; CSS edits for typography

--- public/js/postandretrieve.js
+++ public/js/postandretrieve.js
@@ -10,7 +10,7 @@ var getUsers = function(){
 		var users = user.val();
 		for(name in users){
 			var maker = users[name];
-			var template = "<article class='message-full modal' id='" + name + "'><h2>{{name}}</h2><p class='message-content'>{{message}}</p><span class='cohort'>{{cohort}}</span><br></article><a href='#" + name + "' rel='modal:open'><article class='message-preview'><h2>{{name}}</h2><span class='cohort'>{{cohort}}</span><br></article></a>";
+			var template = "<article class='message-full modal' id='" + name + "'><h2>{{name}}</h2><p class='message-content'>{{message}}</p><span class='cohort'>{{cohort}}</span><br></article><a href='#" + name + "' rel='modal:open'><article class='message-preview' id='" + name + "'><h2>{{name}}</h2><span class='cohort'>{{cohort}}</span><br></article></a>";
 			$('.message-container').prepend(Mustache.render(template, maker));
     }
 	})
